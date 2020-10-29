731af4f8c67ad8ef1ab73d83d35387d0ac859d2f
Goran Mekić <meka@tilda.center>
2020-10-29 16:03:46 +0100

    Use uWSGI in development

--- templates/site.yml.tpl
+++ templates/site.yml.tpl
@@ -6,4 +6,5 @@
   hosts: SERVICE
   roles:
     - onelove-roles.freebsd-common
-    - devel
+    - onelove-roles.freebsd_freenit
+    - onelove-roles.freebsd_freenit_sql
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
 import os
-import socket
 from importlib import import_module
 
 from freenit import create_app
@@ -12,13 +11,9 @@ config_name = os.getenv('FLASK_ENV') or 'default'
 config = configs[config_name]
 application = create_app(config)
 api.create_api(application)
-hostname = socket.gethostname()
-port = os.environ.get('FLASK_PORT', 5000)
-SWAGGER_PATH = f'{config.OPENAPI_URL_PREFIX}{config.OPENAPI_SWAGGER_UI_PATH}'
-SWAGGER_URL = f'http://{hostname}:{port}{SWAGGER_PATH}'
 
 if __name__ == '__main__':
-    print(' * Swagger URL:', SWAGGER_URL)
+    port = os.environ.get('FLASK_PORT', 5000)
     application.run(
         host='0.0.0.0',
         port=port,
